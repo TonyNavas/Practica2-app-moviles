0dec8273fac863d98fb2e5bef830f3a3f262490b
TonyNavas <tonynavas777@gmail.com>
2020-11-26 11:15:37 -0600

    Tipos.kt

---
+++ tipos/tipos.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+// //numeros
+// val int = 123
+// val long = 123456L
+// val double = 12.34
+// val float =  12.32F
+// val hexadecimal = 0xAB
+// val binary = 0b01010101
+
+//  println(" Entero $int, Entero largo $long, para coma flotante $double, para decimales $float, para hexadecimales $hexadecimal, y binario $binary")
+
+ //booleanos
+//  val x = 1
+//  val y = 2
+//  val z = 2
+//  val esTrue = x < y && x < z
+//  val otroTrue = x == y || y == z
+
+//  println("$esTrue,$x, $y, $z \n $otroTrue $x, $y, $z ")
+
+ //cadenas
+val string = "string con \n una línea nueva"
+val rawString = """
+ raw string es útil para
+ cadenas con muchas líneas
+ """
+ println("$string, $rawString")
+}
