d14125c026f62a8d758fb08a575c74bd222f589d
TonyNavas <tonynavas777@gmail.com>
2020-11-25 20:06:30 -0600

    Ejercicio de variables

---
+++ variables/variables.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var nombre = "Geronimo"
+
+    println("mi nombre es $nombre ")
+
+    val apellido = "Navas"
+
+    println("mi apellido es $apellido")
+
+    var edad = "19"
+
+    println("y tengo $edad años")
+}
