031722d722e2aa9ab2702edfd6207c6ac5f3e90f
TonyNavas <tonynavas777@gmail.com>
2020-11-29 12:07:14 -0600

    Ejercicios de if

---
+++ if/if.kt
@@ -0,0 +1,32 @@
+// import java.util.Date;
+fun main()
+{
+// val fecha = Date()
+// val hoy = if (fecha.year == 2020) true else false
+
+// println("la fecha es $hoy")
+
+//EDAD
+// var edad = 19
+// if(edad >= 18)
+// {
+//  println("eres mayor de edad")
+ 
+// } else {
+//  println("aun eres menor")
+// }
+
+println("edad:")
+val edad = Integer.valueOf(readLine())
+if (edad >= 18)
+{
+    println("Es mayor de edad $edad")
+}
+else
+{
+    println("No eres mayor de edad $edad")
+}
+
+
+}
+
