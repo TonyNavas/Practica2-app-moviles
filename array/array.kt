29acb8ed0e5a0af23cb5e8bcb3870290fa966e9e
TonyNavas <tonynavas777@gmail.com>
2020-11-29 12:05:38 -0600

    Ejerccios de array

---
+++ array/array.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+val array = arrayOf(1, 2, 3)
+val elemento1 = array[0]
+val elemento2 = array[1]
+array[2] = 5
+//imprima el array aquí
+val cuadradosPerfectos = Array(10, { k -> k * k })
+Array<out cuadradosPerfectos>.forEach 
+{
+    println("el elemento $$cuadradosPerfectos") 
+}
+}
+
